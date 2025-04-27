b2da956d3f862640fa4268d47804fe68290640b4
Gerald Mulero <gdmasm@gmail.com>
2025-04-27 13:04:11 -0400

    Created the executor.c helper file find_full_path.c, searches through multiple folders until the executable file is found to find the correct working path.

--- shell.h
+++ shell.h
@@ -12,5 +12,7 @@ void executor(char **args);
 void exit_shell(char **args);
 int vuiltin_handler(char **args);
 int print_env(char **args);
+char *build_path(char *dir, char *cmd);
+char *find_full_path(char *command, char **path_dirs);
 
 #endif /* SHELL_H */
