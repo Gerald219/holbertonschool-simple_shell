9ae492496bd12dac4b3806eefabdaffa69de2887
Gerald Mulero <gdmasm@gmail.com>
2025-04-27 01:47:30 -0400

    Created print_env.c file, this file holds the function print_env(void). This function prints all enviroment variablesone by one. It reads the environ through a while loop, while (environ[i]) to verify that there is a valid enviroment variable string. Uses write() instead of printf() to directly print each variable. It stops when it reaches NULL, there are no more enviroment variables

---
+++ print_env.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+#include <unistd.h>
+
+/**
+ * print_env - prints the current enviroment variables
+ * @args: unused but keeping for compatibility
+ *
+ * Return: Always 1 to continue executing shell
+ */
+int print_env(char **args)
+{
+	int i = 0;
+
+	(void)args;
+
+	while (environ[i])
+	{
+		write(STROUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+
+	return (1);
+}
