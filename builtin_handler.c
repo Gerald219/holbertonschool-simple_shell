916aec359d4edcc76d04fc91f4b9aeebf7c5a1e9
Gerald Mulero <gdmasm@gmail.com>
2025-04-27 01:13:06 -0400

    Created builtin_handler.c file, this uses strcmp to compare strings. If the argument is exit or env continues, if not it returns 0 and tries external command instead

---
+++ builtin_handler.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+#include <string.h>
+
+/**
+ * builtin_handler - handles commands like exit & env
+ * @args: array of command arguments (string split into words)
+ *
+ * Return: 1 if a built-in command was executed, 0 if not built-in
+ */
+int builtin_handler(char **args)
+{
+	if (args == NULL || args[0] == NULL)
+		return (0);
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		exit_shell(args);
+		return (1);
+	}
+
+	if (strcmp(args[0], "env") == 0)
+	{
+		print_env();
+		return (1);
+	}
+
+	return (0);
+}
--- shell.h
+++ shell.h
@@ -10,5 +10,7 @@ void display_prompt(void);
 char **parser_input(char *intput);
 void executor(char **args);
 void exit_shell(char **args);
+int vuiltin_handler(char **args);
+int print_env(char **args);
 
 #endif /* SHELL_H */
